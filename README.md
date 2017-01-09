a61d28e15ab7e631012a43e44dd8cd7f7d960094
Paula Salgado <mail@algo.com>
2017-01-09 15:12:13 -0300

    ws

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Menu para demos MC con wss
+al dar de alta en OS crear una variable de entorno HOSTIP con el nombre de host app-proyecto
--- index.js
+++ index.js
@@ -1,37 +1,38 @@
 var fs = require('fs');
 var https = require('https');
-//var WebSocketServer = require('ws').Server;
+var WebSocketServer = require('ws').Server;
 var express = require("express");
 var bodyParser = require("body-parser");
-
+var HOSTIP = process.env.HOSTIP;
 var serverConfig = {
     key: fs.readFileSync('./server.key'),
     cert: fs.readFileSync('./server.crt'),
 };
 
 var app = express();
-var HTTPS_PORT = 8443;
+var HTTPS_PORT = 8080;
 
 var httpsServer = https.createServer(serverConfig, app).listen(HTTPS_PORT);
 
-//var wss = new WebSocketServer({server: httpsServer});
+var wss = new WebSocketServer({server: httpsServer});
 
-//wss.on('connection', function(ws) {
-//    ws.on('message', function(message) {
-//        wss.broadcast(message);
-//    });
-//});
+wss.on('connection', function(wss) {
+	wss.send('conectado');
+    ws.on('message', function(message) {
+        wss.broadcast(message);
+    });
+});
 
-//wss.broadcast = function(data) {
-//    for(var i in this.clients) {
-//        this.clients[i].send(data);
-//    }
-//};
+wss.broadcast = function(data) {
+    for(var i in this.clients) {
+        this.clients[i].send(data);
+    }
+};
 
 app.get(/^(.+)$/, function(req, res){ 
     switch(req.params[0]) {
-        case '/prueba.html':
-            res.send("prueba ok");
+        case '/voice':
+            res.send("selecciono VOICE");
             break;
     default: res.sendFile( __dirname + req.params[0]); 
     }
